39d44981e8b87361c0a05ae8b306345cb82795c9
Sergey Astapov <SergeAstapov@gmail.com>
2022-12-06 14:52:51 -0500

    fix: v1 addon and dummy app compat

--- packages/ember-runtime-config/addon-main.js
+++ packages/ember-runtime-config/addon-main.js
@@ -1,15 +1,26 @@
 const { addonV1Shim } = require('@embroider/addon-shim');
 const middleware = require('./middleware');
+const { join } = require('path');
+
+function isV1Addon(project) {
+  return project.isEmberCLIAddon() && project.pkg['ember-addon'].version !== 2;
+}
+
+function projectRoot(project) {
+  return isV1Addon(project)
+    ? join(project.root, 'tests', 'dummy')
+    : project.root;
+}
 
 module.exports = {
   ...addonV1Shim(__dirname),
 
   serverMiddleware({ app }) {
-    app.use(middleware(this.project.root));
+    app.use(middleware(projectRoot(this.project)));
   },
 
   testemMiddleware(app) {
-    app.use(middleware(this.project.root));
+    app.use(middleware(projectRoot(this.project)));
   },
 
   contentFor(hook) {
