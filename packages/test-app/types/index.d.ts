52c3f31b25ae3cd6d8448fe4f906da42f5ad42de
Sergey Astapov <sergeastapov@gmail.com>
2026-02-24 21:44:52 -0500

    Bump prettier

--- packages/test-app/types/index.d.ts
+++ packages/test-app/types/index.d.ts
@@ -8,8 +8,7 @@ import type EmberRuntimeConfigRegistry from 'ember-runtime-config/template-regis
 declare module '@glint/environment-ember-loose/registry' {
   // Remove this once entries have been added! 👇
   // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
-  export default interface Registry
-    extends EmberRuntimeConfigRegistry /* other addon registries */ {
+  export default interface Registry extends EmberRuntimeConfigRegistry /* other addon registries */ {
     // local entries
   }
 }
