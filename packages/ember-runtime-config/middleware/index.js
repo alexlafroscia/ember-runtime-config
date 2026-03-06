1986f57fc4d111631969c0e7d8135dc6c6e03616
zurnachyag <zurnachyag@ae.com>
2026-03-06 18:26:35 +0400

    introduce etag cache mechanism

--- packages/ember-runtime-config/middleware/index.js
+++ packages/ember-runtime-config/middleware/index.js
@@ -1,20 +1,40 @@
 'use strict';
 
 const theredoc = require('theredoc');
+const crypto = require('crypto');
 const getConfig = require('../lib/get-config.js');
 
-module.exports = function (projectRoot) {
+module.exports = function (projectRoot, useEtag = false) {
   const runtimeConfig = getConfig(projectRoot);
+
   const scriptContent = theredoc`
-    (function(context) {
-      context._erc = ${JSON.stringify(runtimeConfig)};
-    })(window);
-  `;
+(function(context) {
+context._erc = ${JSON.stringify(runtimeConfig)};
+})(window);
+`;
+
+  let etag;
+
+  if (useEtag) {
+    etag = crypto.createHash('sha1').update(scriptContent).digest('hex');
+  }
 
   return (req, res, next) => {
     if (req.path === '/__/env.js') {
       res.set('Content-Type', 'application/javascript');
-      res.set('Cache-Control', 'max-age=0, no-cache, no-store');
+
+      if (useEtag) {
+        res.set('Cache-Control', 'max-age=0, no-cache');
+        res.set('ETag', etag);
+
+        if (req.headers['if-none-match'] === etag) {
+          res.status(304).end();
+          return;
+        }
+      } else {
+        res.set('Cache-Control', 'max-age=0, no-cache, no-store');
+      }
+
       res.send(scriptContent);
     } else {
       next();
